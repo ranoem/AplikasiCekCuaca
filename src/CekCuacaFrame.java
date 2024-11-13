e8504c553e74bba372560e091a349df54f7fad66
dwipa <84449848+ranoem@users.noreply.github.com>
2024-11-13 20:53:04 +0800

    Menambahkan icon untuk beberapa cuaca, menambahkan variasi fitur menambahkan kota favorit ke JComboBox

--- src/CekCuacaFrame.java
+++ src/CekCuacaFrame.java
@@ -1,9 +1,12 @@
 import java.awt.Image;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
 import org.json.JSONObject;
 
 public class CekCuacaFrame extends javax.swing.JFrame {
@@ -14,9 +17,18 @@ public class CekCuacaFrame extends javax.swing.JFrame {
      */
     public CekCuacaFrame() {
         initComponents();
+        
+        cmbKota.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                if (e.getStateChange() == ItemEvent.SELECTED) {
+                    String selectedKota = e.getItem().toString();
+                    txtKota.setText(selectedKota);
+                }
+            }
+        });
     }
     
-    /*
     private void cekCuaca(String kota) {
         try {
             String urlStr = "http://api.openweathermap.org/data/2.5/weather?q=" + kota + "&appid=" + apiKey + "&units=metric";
@@ -39,90 +51,58 @@ public class CekCuacaFrame extends javax.swing.JFrame {
 
             labelCuaca.setText("Cuaca di " + kota + ": " + kondisi + ", Suhu: " + suhu + "°C");
 
-            if (kondisi.equalsIgnoreCase("Clear")) {
-                labelGambar.setIcon(new ImageIcon("sunny.png")); // Path gambar "cerah"
-            } else if (kondisi.equalsIgnoreCase("Clouds")) {
-                labelGambar.setIcon(new ImageIcon("cloudy.png")); // Path gambar "berawan"
-            } else if (kondisi.equalsIgnoreCase("Rain")) {
-                labelGambar.setIcon(new ImageIcon("icon/rain.png")); // Path gambar "hujan"
-            } else {
-                labelGambar.setIcon(new ImageIcon("default.png")); // Path gambar default
+            int labelWidth = 199;
+            int labelHeight = 172;
+            ImageIcon icon = null;
+            switch (kondisi.toLowerCase()) {
+                case "clear":
+                    icon = new ImageIcon("icon/sunny.png"); // Path for clear weather
+                    break;
+                case "clouds":
+                    icon = new ImageIcon("icon/clouds.png"); // Path for cloudy weather
+                    break;
+                case "rain":
+                    icon = new ImageIcon("icon/rain.png"); // Path for rainy weather
+                    break;
+                case "thunderstorm":
+                    icon = new ImageIcon("icon/thunderstorm.png"); // Path for thunderstorm
+                    break;
+                case "fog":
+                case "mist":
+                    icon = new ImageIcon("icon/fog.png"); // Path for foggy weather
+                    break;
+                case "haze":
+                    icon = new ImageIcon("icon/haze.png"); // Path for hazy weather
+                    break;
+                default:
+                    icon = new ImageIcon("default.png"); // Default icon
+                    break;
             }
-            
+
+            if (icon != null) {
+                Image img = icon.getImage();
+                Image scaledImg = img.getScaledInstance(labelWidth, labelHeight, Image.SCALE_SMOOTH);
+                labelGambar.setIcon(new ImageIcon(scaledImg));
+            }
+
+            if (!isKotaExistInComboBox(kota)) {
+                cmbKota.addItem(kota);
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
             labelCuaca.setText("Gagal mendapatkan data cuaca.");
         }
-    }*/
-    
-    private void cekCuaca() {
-    try {
-        // Mengambil nama kota dari txtKota
-        String kota = txtKota.getText().trim();
-        String urlStr = "http://api.openweathermap.org/data/2.5/weather?q=" + kota + "&appid=" + apiKey + "&units=metric";
-        URL url = new URL(urlStr);
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("GET");
-
-        BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        String inputLine;
-        StringBuilder content = new StringBuilder();
-        while ((inputLine = in.readLine()) != null) {
-            content.append(inputLine);
-        }
-        in.close();
-        conn.disconnect();
-
-        JSONObject json = new JSONObject(content.toString());
-        String kondisi = json.getJSONArray("weather").getJSONObject(0).getString("main");
-        double suhu = json.getJSONObject("main").getDouble("temp");
-
-        labelCuaca.setText("Cuaca di " + kota + ": " + kondisi + ", Suhu: " + suhu + "°C");
-
-        // Menentukan ukuran gambar yang diinginkan
-        int labelWidth = 199;
-        int labelHeight = 172;
-
-        // Mengambil gambar sesuai kondisi cuaca
-        ImageIcon icon = null;
-        switch (kondisi.toLowerCase()) {
-            case "clear":
-                icon = new ImageIcon("icon/sunny.png"); // Path gambar "cerah"
-                break;
-            case "clouds":
-                icon = new ImageIcon("icon/clouds.png"); // Path gambar "berawan"
-                break;
-            case "rain":
-                icon = new ImageIcon("icon/rain.png"); // Path gambar "hujan"
-                break;
-            case "thunderstorm":
-                icon = new ImageIcon("icon/thunderstorm.png"); // Path gambar "badai petir"
-                break;
-            case "fog":
-            case "mist":
-                icon = new ImageIcon("icon/fog.png"); // Path gambar "kabut"
-                break;
-            case "haze":
-                icon = new ImageIcon("icon/haze.png"); // Path gambar "haze"
-                break;
-            default:
-                icon = new ImageIcon("default.png"); // Path gambar default
-                break;
-        }
-
-        // Menyesuaikan ukuran gambar yang diinginkan (199x172)
-        if (icon != null) {
-            Image img = icon.getImage();  // Mendapatkan gambar dari ImageIcon
-            Image scaledImg = img.getScaledInstance(labelWidth, labelHeight, Image.SCALE_SMOOTH); // Menyesuaikan ukuran
-            labelGambar.setIcon(new ImageIcon(scaledImg));  // Mengatur gambar yang telah diubah ukurannya
-        }
-
-    } catch (Exception e) {
-        e.printStackTrace();
-        labelCuaca.setText("Gagal mendapatkan data cuaca.");
     }
-}
 
+        private boolean isKotaExistInComboBox(String kota) {
+        for (int i = 0; i < cmbKota.getItemCount(); i++) {
+            if (cmbKota.getItemAt(i).equalsIgnoreCase(kota)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 
     /**
@@ -149,8 +129,6 @@ public class CekCuacaFrame extends javax.swing.JFrame {
         jLabel1.setDisplayedMnemonic('A');
         jLabel1.setFont(new java.awt.Font("Trebuchet MS", 1, 18)); // NOI18N
         jLabel1.setText("Aplikasi Cek Cuaca");
-
-        cmbKota.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Jakarta", "Surabaya", "Bandung", "Medan", "Yogyakarta", "Makassar", "Semarang", "Malang", "Palembang", "Denpasar", "Tangerang", "Batam", "Bali", "Banjarmasin", "Ambon", "Makasar", "Pontianak", "Solo", "Cirebon", "Samarinda" }));
 
         jButton1.setText("Cek");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
@@ -230,7 +208,13 @@ public class CekCuacaFrame extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        cekCuaca();
+        String kota = txtKota.getText().trim();
+    
+        if (kota.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Nama kota tidak boleh kosong.", "Peringatan", JOptionPane.WARNING_MESSAGE);
+        } else {
+            cekCuaca(kota);
+        }
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
